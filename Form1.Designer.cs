1cca926698ee2e44efb58bac282bafa2d4cdf0a4
metaloludek <piotr1989@interia.eu>
2023-06-11 11:48:31 +0200

    Cleaning Project
    Cleaning to fit up kitchen view to Fatlum's changes. Data source has changed, updated method to obtain orders, set the status "ready", remove the status "ready".

---
+++ Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace RestaurantDashboardKitchen
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pictureBox1 = new PictureBox();
+            update_order_status = new Button();
+            date_time_main = new Label();
+            view_orders_button = new Button();
+            dataGridView1 = new DataGridView();
+            Column1 = new DataGridViewTextBoxColumn();
+            Column2 = new DataGridViewTextBoxColumn();
+            Column3 = new DataGridViewTextBoxColumn();
+            Column4 = new DataGridViewTextBoxColumn();
+            Column5 = new DataGridViewTextBoxColumn();
+            Column6 = new DataGridViewTextBoxColumn();
+            Column7 = new DataGridViewTextBoxColumn();
+            timer_now = new System.Windows.Forms.Timer(components);
+            dateTimePicker1 = new DateTimePicker();
+            quit = new Button();
+            cancel_order_status = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = SystemColors.AppWorkspace;
+            pictureBox1.BackgroundImage = (Image)resources.GetObject("pictureBox1.BackgroundImage");
+            pictureBox1.Location = new Point(-4, -9);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(1135, 149);
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // update_order_status
+            // 
+            update_order_status.BackColor = SystemColors.Control;
+            update_order_status.ForeColor = Color.FromArgb(0, 192, 0);
+            update_order_status.Location = new Point(13, 550);
+            update_order_status.Name = "update_order_status";
+            update_order_status.Size = new Size(107, 63);
+            update_order_status.TabIndex = 4;
+            update_order_status.Text = "Update Order Status";
+            update_order_status.UseVisualStyleBackColor = false;
+            update_order_status.Click += update_order_status_Click;
+            // 
+            // date_time_main
+            // 
+            date_time_main.AutoSize = true;
+            date_time_main.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            date_time_main.Location = new Point(13, 171);
+            date_time_main.Name = "date_time_main";
+            date_time_main.Size = new Size(118, 37);
+            date_time_main.TabIndex = 5;
+            date_time_main.Text = "TheTime";
+            // 
+            // view_orders_button
+            // 
+            view_orders_button.BackColor = SystemColors.Control;
+            view_orders_button.Location = new Point(13, 481);
+            view_orders_button.Name = "view_orders_button";
+            view_orders_button.Size = new Size(228, 63);
+            view_orders_button.TabIndex = 4;
+            view_orders_button.Text = "View Orders";
+            view_orders_button.UseVisualStyleBackColor = false;
+            view_orders_button.Click += view_orders_api_button;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.BackgroundColor = SystemColors.Control;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2, Column3, Column4, Column5, Column6, Column7 });
+            dataGridView1.Location = new Point(270, 188);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(824, 486);
+            dataGridView1.TabIndex = 7;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // Column1
+            // 
+            Column1.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Column1.FillWeight = 10.3918686F;
+            Column1.HeaderText = "Column1";
+            Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            Column2.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Column2.FillWeight = 41.3482552F;
+            Column2.HeaderText = "Column2";
+            Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            Column3.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Column3.FillWeight = 17.333025F;
+            Column3.HeaderText = "Column3";
+            Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            Column4.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Column4.FillWeight = 65.8709259F;
+            Column4.HeaderText = "Column4";
+            Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            Column5.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Column5.FillWeight = 20.64853F;
+            Column5.HeaderText = "Column5";
+            Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            Column6.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Column6.FillWeight = 44.6981163F;
+            Column6.HeaderText = "Column6";
+            Column6.Name = "Column6";
+            // 
+            // Column7
+            // 
+            Column7.HeaderText = "Column7";
+            Column7.Name = "Column7";
+            // 
+            // timer_now
+            // 
+            timer_now.Enabled = true;
+            timer_now.Interval = 1000;
+            timer_now.Tick += Timer_now_Tick;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(270, 159);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(200, 23);
+            dateTimePicker1.TabIndex = 8;
+            dateTimePicker1.ValueChanged += dateTimePicker1_ValueChanged;
+            // 
+            // quit
+            // 
+            quit.BackColor = SystemColors.Control;
+            quit.Location = new Point(13, 619);
+            quit.Name = "quit";
+            quit.Size = new Size(227, 55);
+            quit.TabIndex = 9;
+            quit.Text = "Quit";
+            quit.UseVisualStyleBackColor = false;
+            quit.Click += quit_Click;
+            // 
+            // cancel_order_status
+            // 
+            cancel_order_status.BackColor = SystemColors.Control;
+            cancel_order_status.ForeColor = Color.Red;
+            cancel_order_status.Location = new Point(133, 550);
+            cancel_order_status.Name = "cancel_order_status";
+            cancel_order_status.Size = new Size(107, 63);
+            cancel_order_status.TabIndex = 10;
+            cancel_order_status.Text = "Cancel Order Status";
+            cancel_order_status.UseVisualStyleBackColor = false;
+            cancel_order_status.Click += cancel_order_status_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.GradientInactiveCaption;
+            ClientSize = new Size(1106, 686);
+            Controls.Add(cancel_order_status);
+            Controls.Add(quit);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(dataGridView1);
+            Controls.Add(date_time_main);
+            Controls.Add(view_orders_button);
+            Controls.Add(update_order_status);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Restaurant Dashboard - Kitchen View";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private PictureBox pictureBox1;
+        private Button update_order_status;
+        private Label date_time_main;
+        private Button view_orders_button;
+        private System.Windows.Forms.Timer timer_now;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
+        private DataGridViewTextBoxColumn Column3;
+        private DataGridViewTextBoxColumn Column4;
+        private DataGridViewTextBoxColumn Column5;
+        private DataGridViewTextBoxColumn Column6;
+        private DataGridViewTextBoxColumn Column7;
+        public DataGridView dataGridView1;
+        private DateTimePicker dateTimePicker1;
+        private Button quit;
+        private Button cancel_order_status;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,301 @@
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using static RestaurantDashboardKitchen.Program;
+
+namespace RestaurantDashboardKitchen
+{
+    public partial class Form1 : Form
+    {
+        static DateTime date_now = DateTime.Now;
+        static SheetsService service;
+        private string sheetTitle = null;
+        static string SpreadsheetId = "1LTfSmD9ew3rT23FL7Ffw2t92mJlhG-Ept-AR0QE0_1U";
+
+        public Form1()
+        {
+
+            InitializeComponent();
+
+            string[] Scopes = { SheetsService.Scope.Spreadsheets };
+            string ApplicationName = "ApplicationName";
+            GoogleCredential credential;
+
+            /* ---  Access json file path  --- */
+            using (var stream = new FileStream("restaurantdashboard-384320-b3d9c1d86d17.json", FileMode.Open, FileAccess.Read))
+            {
+                credential = GoogleCredential.FromStream(stream)
+                    .CreateScoped(Scopes);
+            }
+            service = new SheetsService(new Google.Apis.Services.BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+        }
+
+        public class apiSheetsRead
+        {
+            DataGridView dataGridView1 = new DataGridView();
+            int orderIdInt;
+
+            static string SpreadsheetId = "1LTfSmD9ew3rT23FL7Ffw2t92mJlhG-Ept-AR0QE0_1U";
+            internal static Order order { get; set; }
+            string sheetTitle = DateTime.Now.ToString("dd/MM/yyyy");
+
+            public void readEntries(string sheet, DataGridView dataGridView1, bool popUpWarn)
+            {
+                var range = $"{sheet}!A:Z";
+                var request = service.Spreadsheets.Values.Get(SpreadsheetId, range);
+                var response = request.Execute();
+                var values = response.Values;
+
+                orderIdInt = dataGridView1.Rows.Count;
+
+                void clearBeforeYouGoAndAddHeaders()
+                {
+                    dataGridView1.Rows.Clear();
+                    dataGridView1.Columns.Clear();
+                    dataGridView1.Columns.Add("ID", "ID");
+                    dataGridView1.Columns.Add("TIME", "TIME");
+                    dataGridView1.Columns.Add("TABLE", "TABLE");
+                    dataGridView1.Columns.Add("ORDER_MENU", "ORDER_MENU");
+                    dataGridView1.Columns.Add("BILL", "BILL");
+                    dataGridView1.Columns.Add("STAFF", "STAFF");
+                    dataGridView1.Columns.Add("STATUS", "STATUS");
+                }
+                void addTheRowsAfterHeaders()
+                {
+                    if (values != null && values.Count > 0)
+                    {
+                        for (int i = 0; i < values.Count; i++)
+                        {
+                            var row = values[i];
+                            dataGridView1.Rows.Add(row.ToArray());
+                        }
+                    }
+                    else if (popUpWarn == true)
+                    {
+                        MessageBox.Show("No data found");
+                    }
+
+
+                }
+
+                clearBeforeYouGoAndAddHeaders();
+                addTheRowsAfterHeaders();
+            }
+
+
+            public void createEntries(string sheet)
+            {
+                string orderStringMenu = "";
+
+                order.ID = orderIdInt;
+                string orderDateHHmmToString = order.OrderDate.ToString("HH:mm");
+
+                string orderTableIDToString = order.TableID.ToString();
+
+                void getOrderMenuAndMakeItString()
+                {
+                    foreach (var item in order.OrderMenu)
+                    {
+                        orderStringMenu += $"- {item.Nazwa} ({item.Cena} z³)\n";
+                    }
+                }
+                getOrderMenuAndMakeItString();
+
+                var range = $"{sheet}!A:Z";
+                var valueRange = new ValueRange();
+                var objectList = new List<object>() { order.ID, orderDateHHmmToString, orderTableIDToString, orderStringMenu, $"{order.Bill} z³", $"{order.Staff.Imie} {order.Staff.Nazwisko}", order.Status };
+                valueRange.Values = new List<IList<object>> { objectList };
+                var appendRequest = service.Spreadsheets.Values.Append(valueRange, SpreadsheetId, range);
+                appendRequest.ValueInputOption = SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum.USERENTERED;
+                var appendReponse = appendRequest.Execute();
+            }
+
+            public bool checkForSheet(DateTime selectedDate)
+            {
+                sheetTitle = selectedDate.ToString("dd/MM/yyyy");
+                var request = service.Spreadsheets.Get(SpreadsheetId);
+                var response = request.Execute();
+                var sheet = response.Sheets.FirstOrDefault(s => s.Properties.Title == sheetTitle);
+
+                if (sheet != null)
+                {
+                    readEntries(sheetTitle, dataGridView1, false);
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+
+            public void createNewDaySheet()
+            {
+                var addSheetRequest = new AddSheetRequest();
+                addSheetRequest.Properties = new SheetProperties();
+                addSheetRequest.Properties.Title = sheetTitle;
+                var addSheetRequestList = new List<Request>();
+                var addSheetRequestContainer = new Request();
+                addSheetRequestContainer.AddSheet = addSheetRequest;
+                addSheetRequestList.Add(addSheetRequestContainer);
+                var batchUpdateRequest = new BatchUpdateSpreadsheetRequest();
+                batchUpdateRequest.Requests = addSheetRequestList;
+                var batchUpdateResponse = service.Spreadsheets.BatchUpdate(batchUpdateRequest, SpreadsheetId);
+                batchUpdateResponse.Execute();
+            }
+        }
+
+        private void Timer_now_Tick(object sender, EventArgs e)
+        {
+            date_time_main.Text = DateTime.Now.ToString("dd/MM/yyyy\nhh:mm:ss tt ");
+        }
+
+
+
+        void view_orders_api_button(object sender, EventArgs e)
+        {
+            apiSheetsRead apiSheetsRead = new apiSheetsRead();
+            try
+            {
+                DateTime selectedDate = dateTimePicker1.Value;
+                bool sheetExists = apiSheetsRead.checkForSheet(selectedDate);
+
+                if (sheetExists)
+                {
+                    apiSheetsRead.readEntries(sheetTitle, dataGridView1, true);
+                }
+                else
+                {
+                    dataGridView1.Rows.Clear();
+                    MessageBox.Show("Brak zamówieñ w wybranym dniu.");
+                }
+            }
+            catch (Google.GoogleApiException ex)
+            {
+                MessageBox.Show("No data found");
+            }
+        }
+
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            DateTime selectedDate = dateTimePicker1.Value;
+            apiSheetsRead apiSheetsRead = new apiSheetsRead();
+            bool sheetExists = apiSheetsRead.checkForSheet(selectedDate);
+
+            if (sheetExists)
+            {
+                sheetTitle = selectedDate.ToString("dd/MM/yyyy");
+                apiSheetsRead.readEntries(sheetTitle, dataGridView1, true);
+                update_order_status.Enabled = sheetTitle == DateTime.Now.ToString("dd/MM/yyyy");
+            }
+            else
+            {
+                dataGridView1.Rows.Clear();
+                dataGridView1.Columns.Clear();
+                MessageBox.Show("Brak zamówieñ w wybranym dniu.");
+                update_order_status.Enabled = false;
+            }
+        }
+
+
+
+
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void update_order_status_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                DataGridViewRow selectedRow = dataGridView1.SelectedRows[0];
+                int orderId = Convert.ToInt32(selectedRow.Cells["ID"].Value);
+                int rowIndex = selectedRow.Index;
+                dataGridView1.Rows[rowIndex].Cells["STATUS"].Value = "Gotowe";
+                UpdateOrderStatusInSheets(orderId, "Gotowe");
+            }
+        }
+
+
+        private void UpdateOrderStatusInSheets(int orderId, string status)
+        {
+            var range = $"{sheetTitle}!A:G";
+            var request = service.Spreadsheets.Values.Get(SpreadsheetId, range);
+            var response = request.Execute();
+            var values = response.Values;
+
+            if (values != null && values.Count > 0)
+            {
+                for (int i = 0; i < values.Count; i++)
+                {
+                    var row = values[i];
+                    if (row.Count >= 7 && row[0] != null && int.TryParse(row[0].ToString(), out int rowOrderId) && rowOrderId == orderId)
+                    {
+                        row[6] = status;
+                        var updateRange = $"{sheetTitle}!A{i + 1}:G{i + 1}";
+                        var updateValueRange = new ValueRange();
+                        updateValueRange.Values = new List<IList<object>> { row };
+                        var updateRequest = service.Spreadsheets.Values.Update(updateValueRange, SpreadsheetId, updateRange);
+                        updateRequest.ValueInputOption = SpreadsheetsResource.ValuesResource.UpdateRequest.ValueInputOptionEnum.USERENTERED;
+                        var updateResponse = updateRequest.Execute();
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void quit_Click(object sender, EventArgs e)
+        {
+            Form1.ActiveForm.Close();
+        }
+
+        private void cancel_order_status_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                int orderId = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["ID"].Value);
+
+                var range = $"{sheetTitle}!A:G";
+                var request = service.Spreadsheets.Values.Get(SpreadsheetId, range);
+                var response = request.Execute();
+                var values = response.Values;
+
+                if (values != null && values.Count > 0)
+                {
+                    for (int i = 0; i < values.Count; i++)
+                    {
+                        var row = values[i];
+                        if (row.Count >= 7 && row[0] != null && int.TryParse(row[0].ToString(), out int rowOrderId) && rowOrderId == orderId)
+                        {
+                            row[6] = "STATUS";
+
+                            var updateRange = $"{sheetTitle}!A{i + 1}:G{i + 1}";
+                            var updateValueRange = new ValueRange();
+                            updateValueRange.Values = new List<IList<object>> { row };
+                            var updateRequest = service.Spreadsheets.Values.Update(updateValueRange, SpreadsheetId, updateRange);
+                            updateRequest.ValueInputOption = SpreadsheetsResource.ValuesResource.UpdateRequest.ValueInputOptionEnum.USERENTERED;
+                            var updateResponse = updateRequest.Execute();
+
+                            break;
+                        }
+                    }
+                }
+            }
+
+            apiSheetsRead sheetsRead = new apiSheetsRead();
+            sheetsRead.readEntries(sheetTitle, dataGridView1, false);
+        }
+
+
+
+
+
+    }
+}
